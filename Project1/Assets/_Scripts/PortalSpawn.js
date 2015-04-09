ae783e8500043d53b15f4732cadd594d4e41ac05
Katharina Kristensen <Katharina_Kristensen@emerson.edu>
2015-04-09 19:56:26 -0400

    Adjust light levels, Add sound

---
+++ Project1/Assets/_Scripts/PortalSpawn.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var makeGate = true;
+var gate : GameObject;
+
+function Start () {
+
+if (makeGate == true){
+
+Instantiate (gate);
+
+makeGate = false;
+
+}
+
+}
+
+function Update () {
+
+
+}
