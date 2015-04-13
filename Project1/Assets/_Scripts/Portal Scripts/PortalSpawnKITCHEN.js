abba5a4c78cb7e60dbf7ade294837b2fdd3c19cb
Katharina Kristensen <Katharina_Kristensen@emerson.edu>
2015-04-13 10:25:22 -0400

    Add portal spawn script

---
+++ Project1/Assets/_Scripts/Portal Scripts/PortalSpawnKITCHEN.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var makeGate = true;
+
+var gate : GameObject;
+
+function Start () {
+
+	if (makeGate == true){
+		Instantiate (gate);
+		makeGate = false;
+}
+}
+
+function Update () {
+}
