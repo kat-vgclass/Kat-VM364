69ca46dd0863d382963f7c329d798a42a0620d6d
Katharina Kristensen <Katharina_Kristensen@emerson.edu>
2015-04-14 20:39:08 -0400

    Add lamps, Add bedroom furniture plus livingroom object, Add GoalTowers, Add faulty GoalTower script, Need fix destroy instantiated towers from prefab

---
+++ Project1/Assets/_Scripts/TowerSpawnScript.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var makeTower = true;
+
+var newTower : GameObject;
+
+var oldTower : GameObject;
+
+
+
+function OnTriggerEnter (other : Collider) {
+
+	if (makeTower == true){
+	
+		Instantiate (newTower);
+		
+		Destroy (oldTower);
+		
+		makeTower = false;
+}
+}
+
